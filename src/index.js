380b293b83dc15d1ea60b5c114908836c1d51cb0
daniel <daanduque@utp.edu.co>
2021-11-09 08:41:20 -0500

    ready to deploy on github pages

--- src/index.js
+++ src/index.js
@@ -1,17 +1,144 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+//Cada Cuadrado del tablero
+const Square = ({ value, onClick }) => {
+
+  return (
+    <button
+      className="square"
+      onClick={onClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+
+// Tablero con los 9 cuadrado
+const Board = props => {
+  
+  
+  const renderSquare = (i) => {
+    return (
+      <Square
+        value={props.squares[i]}
+        onClick={() => props.onClick(i)}
+      />
+    );
+  }
+
+    return (
+      <div>
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+
+
+// Tablero y el historial del juego
+const FunctionalGame = props => {
+
+  const [history, setHistory] = useState([{squares: Array(9).fill(null)}]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClick = (i) => {
+    const localHistory = history.slice(0, stepNumber + 1);
+    const current = localHistory[localHistory.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = xIsNext ? 'X' : 'O';
+    setHistory([ ...localHistory, { squares } ]);
+    setStepNumber(localHistory.length);
+    setXIsNext(!xIsNext);
+  }
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXIsNext((step % 2) === 0);
+  }
+
+    const localHistory = history;
+    const current = localHistory[stepNumber]
+    const winner = calculateWinner(current.squares);
+
+    const moves = localHistory.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+
+    let status = '';
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={ current.squares }
+            onClick={ handleClick }
+          />
+        </div>
+        <div className="game-info">
+          <div>{ status }</div>
+          <ol>{ moves }</ol>
+        </div>
+      </div>
+    );
+  }
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <FunctionalGame />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
